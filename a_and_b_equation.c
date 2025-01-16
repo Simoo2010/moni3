99e37d4d4ececde66dd9a62bc16d63aec1f455d1
Simeon Velikov <veikov.simo@gmail.com>
2025-01-16 09:59:47 +0200

    test2

---
+++ a_and_b_equation.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+ float a,b,x;
+ printf("Enter the value of a: ");
+ scanf("%f",&a);
+ printf("proba ");
+ scanf("%f",&b);
+ if(a!=0)
+ {
+  x=-b/a;
+  printf("The solution is %.1fx + %.1f = 0 is x = %.1f \n",a,b,x);
+ }
+ else if(a==0&&b==0)
+ {
+  printf("for 0x + 0 = 0 every x is true \n");
+ }
+ else
+ {
+  printf("no solution \n");
+ }
+ return 0;
+}
